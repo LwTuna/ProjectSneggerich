70076c7edb5cbefe7ac78a6dd04882d1181da69c
BuildTools <unconfigured@null.spigotmc.org>
2019-04-03 22:55:51 +0200

    InitProject

---
+++ ProjectSnegge/src/com/wipdev/snegge/SneggenPlugin.java
@@ -0,0 +1,13 @@
+package com.wipdev.snegge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SneggenPlugin extends JavaPlugin{
+
+	public void onEnable() {
+	}
+	
+	public void onDisable() {
+	}
+	
+}
