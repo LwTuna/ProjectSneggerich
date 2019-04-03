62b09281c28e35b8789d2983812f369603103cd8
BuildTools <unconfigured@null.spigotmc.org>
2019-04-03 23:46:51 +0200

    Changed src level of project setup. Added simple ChatEventHandler Class

---
+++ ProjectSnegge/src/com/wipdev/snegge/chat/ChatEventHandler.java
@@ -0,0 +1,14 @@
+package com.wipdev.snegge.chat;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatEventHandler implements Listener{
+
+	@EventHandler
+	public void onChatEvent(AsyncPlayerChatEvent ce){
+		
+	}
+	
+}
