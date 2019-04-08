0ed564d334383a8b096e1701c48e2abba6c8acf1
Dannnis <ichduer1999@gmail.com>
2019-04-08 22:11:52 +0200

    Elfs ?

---
+++ ProjectSnegge/src/com/wipdev/snegge/races/ElfRace.java
@@ -0,0 +1,32 @@
+package com.wipdev.snegge.races;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import com.wipdev.snegge.items.ItemUtils;
+
+public class ElfRace extends Race{
+
+	public ElfRace() {
+		super("Elf", ChatColor.LIGHT_PURPLE+"[Elf]",ItemUtils.createItem(Material.BOW, 1, "[Elf]"));
+	}
+
+	@Override
+	public void onAbility(PlayerInteractEvent event) {
+		event.getPlayer().getWorld().playEffect(event.getPlayer().getLocation(),Effect.SMOKE,2);
+		event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1));
+	}
+
+	@Override
+	public void applyPermEffects(Player player) {
+		player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 1));
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
+		
+	}
+
+}
