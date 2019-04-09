b87aa5b3bbd1b4489e03cd05ec59082ac84dfecc
LwTuna <jonasreschule@web.de>
2019-04-09 12:37:43 +0200

    fixed and finished deoldra class

--- ProjectSnegge/src/com/wipdev/snegge/races/DeoldraRace.java
+++ ProjectSnegge/src/com/wipdev/snegge/races/DeoldraRace.java
@@ -1,5 +1,6 @@
 package com.wipdev.snegge.races;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -8,6 +9,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import com.wipdev.snegge.SneggenPlugin;
 import com.wipdev.snegge.items.ItemUtils;
 
 public class DeoldraRace extends Race{
@@ -19,12 +21,20 @@ public class DeoldraRace extends Race{
 	}
 
 	@Override
-	public void onAbility(PlayerInteractEvent event) {
+	public void onAbility(final PlayerInteractEvent event) {
 		Player player = event.getPlayer();
-		event.getPlayer().getWorld().playEffect(event.getPlayer().getLocation(),Effect.IRON_DOOR_CLOSE,2);
+		event.getPlayer().getWorld().playEffect(event.getPlayer().getLocation(),Effect.ANVIL_LAND,2);
+		player.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
 		player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, duration*20, 2));
 		player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, duration*20, 2));
 		player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, duration*20, 0));
+		Bukkit.getScheduler().scheduleSyncDelayedTask(SneggenPlugin.instance, new Runnable() {
+			
+			public void run() {
+				applyPermEffects(event.getPlayer());
+				
+			}
+		}, duration*20 +5);
 	}
 
 	@Override
